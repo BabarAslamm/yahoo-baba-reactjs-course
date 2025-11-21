b0de9f41c9e9558e868753f22ff4c29c4a105230
hafizSiddiq7675 <hafiz.siddiq.umer@gmail.com>
2025-11-21 17:27:30 +0500

    Todo App completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Counter from './Counter'
+import Todo from './Todo'
 
 function App() {
 
   return (
     <>
-      <Counter />
+      <Todo />
     </>
   )
 }
---
+++ src/Todo.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+
+function Todo() {
+
+    const [newTodo, setNewTodo] = useState('');
+    const [todos, seTodos] = useState([]); // <-- start with an empty array
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (newTodo.trim() === '') return;
+
+        seTodos([...todos, { text: newTodo, is_completed: false }]);
+        setNewTodo('');
+
+        
+    }
+    
+    const handleDelete = (index) => {
+
+        const newTodos = [...todos];
+        console.log('delete', index)
+        console.log('delete', todos[index])
+        newTodos[index].is_completed = !newTodos[index].is_completed;
+
+        seTodos(newTodos);
+
+    } 
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <h1>Enter New Todo</h1>
+                <label>Todo : </label><br />
+                <input
+                    type="text"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                />
+                <br /><br />
+                <button type="submit">Submit</button>
+            </form>
+
+            <h3>List</h3>
+
+            {todos.map((todo, index) => (
+                <li 
+                 key={index}
+                 style={{ textDecoration : todo.is_completed ? 'line-through' : ' '}}
+                >
+                    {todo.text}
+
+                    <button onClick={() => handleDelete(index)}> 
+                        
+                        {todo.is_completed ?   'UnDo' : 'Done'}
+                        </button>
+                </li>
+            ))}
+
+            
+        </>
+    );
+}
+
+export default Todo;
